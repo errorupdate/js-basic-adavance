4e70789b765a1965799b5fa3ef249c1edfe16505
Uttam Kumar <124666204+errorupdate@users.noreply.github.com>
2025-07-29 15:11:19 +0530

    02 and 03 commit

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,2 +1,19 @@
-console.log("Uttam");
-console.log("Raaz");
+// Investigation Study
+// Variable data types
+const accountId = 13122; // easy to read/ increase readability
+let accountEmail = "uttam@google.com"; // very widely used
+var accountPassword = "3534356"; // not used now a days
+accountCity = "Patna"; // bad practice
+// accountId = 2 // cannot be change because const cannot be changed
+// table form of print console.log
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+const account = {
+  ID: accountId,
+  Email: accountEmail,
+  Password: accountPassword,
+  City: accountCity,
+};
+
+console.table(account);
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js as newer version
+// focus on code readability
+// alert("hello"); // We are using node not the browser environment
+let Name = "uttam" // String
+let age = 24 // number
+let is18plus = true // bool
+
+console.table([Name,age,is18plus])
+
+/* NOTE: 
+Number = 2 power 53
+bigint
+string = ""
+boolean = haa ya na
+undefined
+symbol --> use in react to check uniqueness
+typeof(null) --> object
+typeof(undefined) --> object
+*/
+
+console.table([typeof(Name),typeof(age),typeof(is18plus)]);
