451a167ffbccac26b7475c17a7de40ae36d3b469
Uttam Kumar <124666204+errorupdate@users.noreply.github.com>
2025-07-30 10:48:08 +0530

    Math and num

---
+++ 01_basics/06_numsandMath.js
@@ -0,0 +1,34 @@
+const score = 400
+const balance = new Number(100)
+
+// console.log([score,typeof(score),balance,typeof(balance)])
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_VALUE)
+
+// ++++++++++++++++++++++++ Maths +++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3)); // top value
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+
+
+// console.log(((Math.random()*10) + 1).toPrecision(1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ min)
